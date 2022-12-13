2ccf0d0fbf43e95ac698bace8d2fefe5e7115143
Sean Eric Thomas <set@seanericthomas.com>
2022-12-13 12:00:33 -0800

    Create pseudocode to describe algorithm

---
+++ script.js
@@ -0,0 +1,9 @@
+// A simple script for playing rock-paper-scissors against the computer
+
+// * PSEUDOCODE *
+// Generate a random choice for the computer player
+// Get the player's choice
+// Compare the two choices to see who won
+// Show the player the results of the current round
+// Continue play until five rounds have elapsed
+// Declare a final winner based on all rounds played
