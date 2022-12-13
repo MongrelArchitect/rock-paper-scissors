5524ac311f52faa2371fcec764dce76de6a155f5
Sean Eric Thomas <set@seanericthomas.com>
2022-12-13 13:45:30 -0800

    Generate a random choice for computer player

--- script.js
+++ script.js
@@ -2,6 +2,24 @@
 
 // * PSEUDOCODE *
 // Generate a random choice for the computer player
+function getComputerChoice() {
+  const randomNumber = Math.floor(Math.random() * 3);
+  switch (randomNumber) {
+    case 0:
+      return 'rock';
+      break;
+    case 1:
+      return 'paper';
+      break;
+    case 2:
+      return 'scissors';
+      break;
+    default:
+      return null;
+      break;
+  } 
+}
+
 // Get the player's choice
 // Compare the two choices to see who won
 // Show the player the results of the current round
